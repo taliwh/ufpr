ceb60094093de8391944dd8c7261a5a064842120
Thalísia Arianna Fernandes Fleck <taliwhff@gmail.com>
2025-09-13 23:23:56 -0300

    Update exer11.pas

--- alg 1/lista 2/exer11.pas
+++ alg 1/lista 2/exer11.pas
@@ -1,76 +1,48 @@
-Faça um programa Pascal que leia do teclado um conjunto de 4 valores i, a, b, c, sendo que i é um valor inteiro e positivo e a, b, c, são quaisquer valores reais. Imprima na tela os valores de a, b, c da seguinte forma:
+Um vendedor necessita de um programa que calcule o preço total devido por um cliente que comprou um produto em sua loja. Faça um programa Pascal que receba dois números inteiros que são, respectivamente, o código do produto e a quantidade comprada. Imprima na tela o preço total com duas casas decimais, usando a tabela abaixo.
+Caso o código não exista o programa deve imprimir ERRO.
 
-os três valores a, b, c em ordem crescente, se i = 1
-os três valores a, b, c em ordem decrescente, se i = 2,
-os três valores a, b, c de forma que o maior dentre a, b, c fique entre os outros dois valores, com a ordem relativa entre os outros dois deles mantida, se i = 3. Observe no exemplo de teste 2 que o maior valor (68) foi impresso na saída esperada no meio, e os valores 5 e 45 ficaram na mesma ordem relativa, isto é, o 5 aparece antes do 45 na saída porquê na entrada eles estavam nesta ordem.
+Código do Produto	Preço unitário
+1001	5,32
+1324	6,45
+6548	2,37
+987	5,32
+7623	6,45
 Exemplos:
-
 Exemplo 1:
-1 34 12 21
-
+1324 6
 Saida Esperada 1:
-12 21 34
+38.70
 
 Exemplo 2:
-3 68 5 45
-
+987 9
 Saida Esperada 2:
-5 68 45
+47.88
 
-program OrganizaValores;
+program CalculaPreco;
 var
-  i: integer;
-  a, b, c, maior: real;
+  codigo, quantidade: integer;
+  preco, total: real;
 
 begin
-
-  read(i, a, b, c);
-
-
-  if i = 1 then
+  read(codigo, quantidade);
+  if codigo = 1001 then
+    preco := 5.32
+  else if codigo = 1324 then
+    preco := 6.45
+  else if codigo = 6548 then
+    preco := 2.37
+  else if codigo = 987 then
+    preco := 5.32
+  else if codigo = 7623 then
+    preco := 6.45
+  else
   begin
-
-    if (a <= b) and (b <= c) then
-      writeln(a:0:0, ' ', b:0:0, ' ', c:0:0)
-    else if (a <= c) and (c <= b) then
-      writeln(a:0:0, ' ', c:0:0, ' ', b:0:0)
-    else if (b <= a) and (a <= c) then
-      writeln(b:0:0, ' ', a:0:0, ' ', c:0:0)
-    else if (b <= c) and (c <= a) then
-      writeln(b:0:0, ' ', c:0:0, ' ', a:0:0)
-    else if (c <= a) and (a <= b) then
-      writeln(c:0:0, ' ', a:0:0, ' ', b:0:0)
-    else
-      writeln(c:0:0, ' ', b:0:0, ' ', a:0:0)
-  end
-  else if i = 2 then
-  begin
-
-    if (a >= b) and (b >= c) then
-      writeln(a:0:0, ' ', b:0:0, ' ', c:0:0)
-    else if (a >= c) and (c >= b) then
-      writeln(a:0:0, ' ', c:0:0, ' ', b:0:0)
-    else if (b >= a) and (a >= c) then
-      writeln(b:0:0, ' ', a:0:0, ' ', c:0:0)
-    else if (b >= c) and (c >= a) then
-      writeln(b:0:0, ' ', c:0:0, ' ', a:0:0)
-    else if (c >= a) and (a >= b) then
-      writeln(c:0:0, ' ', a:0:0, ' ', b:0:0)
-    else
-      writeln(c:0:0, ' ', b:0:0, ' ', a:0:0)
-  end
-  else if i = 3 then
-  begin
-
-    maior := a;
-    if b > maior then maior := b;
-    if c > maior then maior := c;
-
-    if (maior = a) then
-      writeln(b:0:0, ' ', a:0:0, ' ', c:0:0)
-    else if (maior = b) then
-      writeln(a:0:0, ' ', b:0:0, ' ', c:0:0)
-    else
-      writeln(a:0:0, ' ', c:0:0, ' ', b:0:0);
+    writeln('ERRO');
+    halt;
   end;
+
+
+  total := preco * quantidade;
+
+  writeln(total:0:2);
 end.
