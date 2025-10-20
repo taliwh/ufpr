4a76f630a5b73cf6cc5ed8445c29cc93a655807b
Thalísia Arianna Fernandes Fleck <taliwhff@gmail.com>
2025-10-20 14:45:27 -0300

    Update exer2.pas

--- alg1/lista 3/exer2.pas
+++ alg1/lista 3/exer2.pas
@@ -1,1 +1,17 @@
+(* 
+Faça um programa Pascal que calcule o valor da soma dos quadrados dos primeiros 50 inteiros positivos não nulos e imprima o resultado do cálculo na tela.
+Observe que este programa não tem entrada, apenas saída. *)
 
+program somatorio;
+var
+    i, soma, num: longint;
+
+begin
+    soma:= 0;
+    for i:= 1 to 50 do
+    begin
+        num:= i * i;
+        soma:= soma + num;
+    end;
+    writeln(soma);
+end.
